2af0d74fe4bcf35b98acade373028e6548e5458d
Helen Emerson <helen@helephant.com>
2018-06-03 22:48:39 +0100

    Unravelled the mysteries of setting nuget properties without a nuspec file

--- build/build-package.ps1
+++ build/build-package.ps1
@@ -2,4 +2,4 @@
 $packageVersion =  ("1.1.0."  + [int](new-timespan -start 01-06-2018 -end (get-date)).TotalMinutes)
 #$packageVersion =  "1.1.0.10" 
 
-dotnet pack  ../src/gem/gem.csproj -o Packages --include-symbols --include-source --configuration release /p:Version=$packageVersion
+dotnet pack  ../src/gem/gem.csproj -o ..\..\build\Packages --include-symbols --include-source --configuration release /p:Version=$packageVersion
